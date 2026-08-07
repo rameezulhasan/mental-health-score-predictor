2b80047422a10b0c3826d6b180093ac526750a14
Rameez Ul Hassan <muhammadrameezhasan@gmail.com>
2026-08-07 10:43:15 +0500

    mental health score predictor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://13.60.43.126:8000',
   timeout: 15000,
   headers: { 'Content-Type': 'application/json' },
 });
